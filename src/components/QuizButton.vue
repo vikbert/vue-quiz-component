acdea87377ccdbafae685e5b222ab328f227512d
Xun Zhou <xun.zhou@lidl.com>
2019-03-12 13:19:44 +0100

    add components QuizButton and QuizEnd

---
+++ src/components/QuizButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <li>
+        <button v-on:click="handleClickAnswerOption">{{ button_text }}</button>
+    </li>
+</template>
+
+<script>
+  export default {
+    name: "QuizButton",
+    props: ['button_text'],
+    methods: {
+      handleClickAnswerOption() {
+        this.$parent.handleClickAnswerOption();
+      },
+    },
+  };
+</script>
+
+<style scoped>
+    button {
+        padding: 10px 50px;
+        margin: 5px;
+    }
+</style>
---
+++ src/components/QuizEnd.vue
@@ -0,0 +1,22 @@
+<template>
+    <div className='quiz-end'>
+        <p>Thanks for playing!</p>
+        <a href="#" v-on:click="handleClick">Reset Quiz</a>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "QuizEnd",
+    props: ['resetQuestionIndexHandler'],
+    methods: {
+      handleClick() {
+        this.resetQuestionIndexHandler()
+      }
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
