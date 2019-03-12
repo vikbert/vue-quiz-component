30c4b8af206357bd68af52cbb602d8f96dcd4a29
Xun Zhou <xun.zhou@lidl.com>
2019-03-12 13:19:07 +0100

    add component Quiz

---
+++ src/components/Quiz.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <QuizEnd v-if="isQuizEnd()" :resetQuestionIndexHandler="resetQuestionIndex"></QuizEnd>
+        <QuizQuestion v-else :question="quiz_data.quiz_questions[quiz_position - 1]"></QuizQuestion>
+    </div>
+</template>
+
+<script>
+  import QuizQuestion from './QuizQuestion.vue'
+  import QuizEnd from './QuizEnd.vue'
+  import json from './quiz_data.json'
+
+  export default {
+    name: "Quiz",
+    data() {
+      return {
+        quiz_data: json,
+        quiz_position: 1,
+        quiz_length: json.quiz_questions.length
+      }
+    },
+    components: {
+      QuizQuestion,
+      QuizEnd
+    },
+    methods: {
+      resetQuestionIndex() {
+        this.quiz_position = 1
+      },
+      incrementQuestionIndex() {
+        this.quiz_position += 1
+      },
+      isQuizEnd() {
+        return (this.quiz_position - 1) === this.quiz_length
+      },
+    },
+  };
+</script>
+
+<style scoped>
+</style>
